a6acadcf6dd16ed8e7e2709fb909d9bd6e581a54
Fih <124883284+Firene11@users.noreply.github.com>
2023-09-10 19:47:16 -0300

    added code to spec, crud - see notes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,26 +1,31 @@
-/*
-/api/users
+const router = require('express').Router();
 
-GET all users
+const {
+  getAllUsers, 
+  getSingleUser, 
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userControllers');
 
-GET a single user by its _id and populated thought and friend data
+router.get('/', getAllUsers); // GET all users
+router.get('/:id', getSingleUser); // GET a single user by its _id and populated thought and friend data
+router.post('/', createUser); // POST a new user
 
-POST a new user:
-
-// example data
+/*example data
 {
-  "username": "lernantino",
-  "email": "lernantino@gmail.com"
-}
-PUT to update a user by its _id
-
-DELETE to remove user by its _id
-
-BONUS: Remove a user's associated thoughts when deleted.
-
-/api/users/:userId/friends/:friendId
-
-POST to add a new friend to a user's friend list
-
-DELETE to remove a friend from a user's friend list
+    "username": "lernantino",
+    "email": "lernantino@gmail.com"
+  }
 */
+
+router.put('/:id', updateUser); // PUT to update a user by its _id
+router.delete('/:id', deleteUser); // DELETE to remove user by its _id
+
+router.post('/:userId/friends/:friendId', addFriend); // POST to add a new friend to a user's friend list
+router.delete('/:userId/friends/:friendId', removeFriend); // DELETE to remove a friend from a user's friend list
+router.delete('/:userId/thoughts/:thoughtId'); // BONUS: Remove a user's associated thoughts when deleted.
+
+module.exports = router;
