c5cdf57d87b8f4e66ac5cf4cc072d261773ac566
Fih <124883284+Firene11@users.noreply.github.com>
2023-09-10 20:31:50 -0300

    added get single thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,13 +3,20 @@ const { User, Thought } = require('../models/index');
 module.exports = {
     async getAllThoughts(req, res) {
         try {
-            const thought = await Thought.find();
+            const thoughts = await Thought.find();
             res.json(thoughts);
         } catch (err) {
             res.status(500).json(err);
         }
     },
-
+    async getSingleThought(req, res) {
+        try {
+          const thought = await Thought.findById(req.params.id);
+          res.json(thought);
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      }
 
 
 };
