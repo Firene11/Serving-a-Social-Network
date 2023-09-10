8398a05854bdf24a0c82b8532d66cfc43594ff51
Fih <124883284+Firene11@users.noreply.github.com>
2023-09-10 18:59:17 -0300

    added code to user model as per spec in challenge - see psuedo notes

--- models/User.js
+++ models/User.js
@@ -1,24 +1,52 @@
-//User
+const { Schema, model } = require('mongoose');
 
-//username
-
-/*String
-Unique
-Required
-Trimmed
-email*/
-
-/*String
-Required
-Unique
-Must match a valid email address (look into Mongoose's matching validation)*/
-
-//thoughts
-
-/*Array of _id values referencing the Thought model*/
-
-//friends
-
-//Array of _id values referencing the User model (self-reference) Schema Settings
-
-//Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+//User - username, String, Unique, Required, Trimmed
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        unique: true,
+        required: true,
+        trimmed: true,
+      },
+//email - String, Required, Unique
+//Must match a valid email address (look into Mongoose's matching validation)
+      email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [
+          /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+          'Please enter a valid email address.',
+        ],
+      },
+//thoughts - Array of _id values referencing the Thought model
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'thought',
+        },
+      ],
+//friends - Array of _id values referencing the User model (self-reference) Schema Settings
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'user',
+        },
+      ],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }
+  );
+  //Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+  userSchema.virtual('friendCount').get(function () {
+    return this.friends ? this.friends.length : 0;
+  });
+  
+  const User = model('user', userSchema);
+  
+  module.exports = User;
