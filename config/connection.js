e2e412451e091ad331d98b086cc83d6b200401da
Fih <124883284+Firene11@users.noreply.github.com>
2023-09-10 17:57:17 -0300

    created a new package.json file, installed proper npm's

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,6 @@
 
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://127.0.0.1:27017/videosAndResponses');
+connect('mongodb://127.0.0.1:27017/mysocialdb');
 
 module.exports = connection;
---
+++ models/Thought.js
@@ -0,0 +1,11 @@
+// require mongoose
+
+// Create a new instance of the Mongoose schema to define shape of each document
+
+    // Add individual properties and their types
+    // Setting required to true will disallow null values
+
+// Using mongoose.model() to compile a model based on the schema
+// 'Thought' is the name of the model
+// created Schema above is the name of the schema we are using to create a new instance of the model
+
