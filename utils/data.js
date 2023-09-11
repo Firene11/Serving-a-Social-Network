8b74c2453a801eec4648ea9ca06132729422a821
Fih <124883284+Firene11@users.noreply.github.com>
2023-09-10 22:10:50 -0300

    trying to get the module exports working between data and seed files

--- utils/data.js
+++ utils/data.js
@@ -16,3 +16,125 @@ example thought data
   "userId": "5edff358a0fcb779aa7b118b"
 }
  */
+
+const someUsers = [
+    {
+        "username": "lernantino",
+        "email": "lernantino@gmail.com"
+    },
+    {
+        "username": "daniel",
+        "email": "daniel@gmail.com"
+    },
+    {
+        "username": "terence",
+        "email": "terence@outlook.com"
+    },
+    {
+        "username": "sarah",
+        "email": "sarah@hotmail.com"
+    },
+    {
+        "username": "jacob",
+        "email": "jacob@gmail.com"
+    },
+    {
+        "username": "aviad",
+        "email": "aviad@netwoking.com"
+    },
+    {
+        "username": "ben",
+        "email": "benben@gmail.com"
+    },
+    {
+        "username": "chris",
+        "email": "christopher@usermail.com"
+    },
+    {
+        "username": "midge",
+        "email": "midgiecat@catsnow.com"
+    },
+  ];
+
+const someThoughts = [
+    {
+        "thoughtText": "Here's a cool thought...",
+        "username": "lernantino",
+        "userId": "5edff358a0fcb779aa7b118b"
+      },
+      {
+        "thoughtText": "When ther's a will, there's a way!",
+        "username": "daniel",
+        "userId": ""
+      },
+      {
+        "thoughtText": "Is the moon made of cheese?",
+        "username": "terence",
+        "userId": ""
+      },
+      {
+        "thoughtText": "I wish summer would never end",
+        "username": "sarah",
+        "userId": ""
+      },
+      {
+        "thoughtText": "I am a pretty smart guy.",
+        "username": "jacob",
+        "userId": ""
+      },
+      {
+        "thoughtText": "What the heck is happening today?",
+        "username": "aviad",
+        "userId": ""
+      },
+      {
+        "thoughtText": "Cats are hilarious",
+        "username": "ben",
+        "userId": ""
+      },
+      {
+        "thoughtText": "I am a man of mystery",
+        "username": "chris",
+        "userId": ""
+      },
+      {
+        "thoughtText": "I will master the universe",
+        "username": "midge",
+        "userId": ""
+      },
+];
+
+const users = [];
+
+// Function to generate random users that we can add to the database. Includes user thoughts.
+const getRandomUser = (int) => {
+    let results = [];
+    for (let i = 0; i < int; i++) {
+      results.push({
+        description: getRandomArrItem(someUsers),
+        thoughts: [...getUserThoughts(1)],
+      });
+    }
+    return results;
+  };
+  
+  // Create the responses that will be added to each user
+  const getRandomThought = (int) => {
+    if (int === 1) {
+      return getRandomArrItem(someThoughts);
+    }
+    let results = [];
+    for (let i = 0; i < int; i++) {
+      results.push({
+        responseBody: getRandomArrItem(someThoughts),
+        username: getRandomUser(),
+      });
+    }
+    return results;
+  };
+  
+
+
+// Export the functions for use in seed.js
+module.exports = { getRandomUser, getRandomThought };
+  
--- utils/seed.js
+++ utils/seed.js
@@ -1,5 +1,36 @@
 const connection = require('../config/connection');
-const { Users, Thoughts } = require('../models');
+const { User, Thought } = require('../models');
 const { getRandomUser, getRandomThought } = require('./data');
 
 connection.on('error', (err) => err);
+
+// Start the seeding runtime timer
+console.time('seeding');
+
+connection.once('open', async () => {
+    console.log('connected');
+    // Delete the collections if they exist
+    let userCheck = await connection.db.listCollections({ name: 'user' }).toArray();
+    if (userCheck.length) {
+      await connection.dropCollection('user');
+    }
+  
+    let thoughtCheck = await connection.db.listCollections({ name: 'thought' }).toArray();
+    if (thoughtCheck.length) {
+      await connection.dropCollection('thought');
+    }
+  
+    const users = [];
+    const thoughts = getRandomThoughts(10);
+
+  
+    await User.collection.insertMany(users);
+    await Thought.collection.insertMany(thoughts);
+  
+    // loop through the saved users, for each user we need to generate a user thought and insert the thoughts
+    console.table(users);
+    console.table(thoughts);
+    console.info('Seeding complete!');
+    process.exit(0);
+  });
+  
