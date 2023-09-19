14993a99909a8dc2a1c3abdb2f90f9f1af5217c1
Fih <124883284+Firene11@users.noreply.github.com>
2023-09-19 16:35:45 -0300

    cleaned up unused code in user and thought routes, was just a thought process

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -13,29 +13,10 @@ const {
 router.route('/').get(getAllThoughts).post(createThought);
 router.route('/:id').get(getSingleThought);
 
-//router.get('/', getAllThoughts); // GET to get all thoughts
-//router.get('/:id', getSingleThought); // GET to get a single thought by its _id
-//router.post('/', createThought); // POST to create a new thought
-// (don't forget to push the created thought's _id to the associated user's thoughts array field)
-
-/*example data
-{
-    "thoughtText": "Here's a cool thought...",
-    "username": "lernantino",
-    "userId": "5edff358a0fcb779aa7b118b"
-  }
-  */
-
 router.route('/:id').put(updateThought);
 router.route('/:id').delete(deleteThought);
 router.route('/thoughtId/reactions').post(createReaction);
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
-
-//router.put('/:id', updateThought); // PUT to update a thought by its _id
-//router.delete('/:id', deleteThought); // DELETE to remove a thought by its _id
-
-//router.post('/:thoughtId/reactions', createReaction); // POST to create a reaction stored in a single thought's reactions array field
-//router.delete('/:thoughtId/reactions/:reactionId', deleteReaction); // DELETE to pull and remove a reaction by the reaction's reactionId value
 
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -16,16 +16,4 @@ router.route(':id').put(updateUser).delete(deleteUser);
 router.route('/:userId/friends/:friendId').post(addFriend);
 router.route('/:userId/friends/:friendId').delete(removeFriend);
 
-//router.get('/', getAllUsers); // GET all users
-//router.get('/:id', getSingleUser); // GET a single user by its _id and populated thought and friend data
-//router.post('/', createUser); // POST a new user
-
-
-//router.put('/:id', updateUser); // PUT to update a user by its _id
-//router.delete('/:id', deleteUser); // DELETE to remove user by its _id
-
-//router.post('/:userId/friends/:friendId', addFriend); // POST to add a new friend to a user's friend list
-//router.delete('/:userId/friends/:friendId', removeFriend); // DELETE to remove a friend from a user's friend list
-//router.delete('/:userId/thoughts/:thoughtId'); // BONUS: Remove a user's associated thoughts when deleted.
-
 module.exports = router;
